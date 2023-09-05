7a9dc59629de1ad9b352ece788632288b4ffa729
RappyTV <rappytv@rappytv.com>
2023-09-05 21:07:07 +0200

    Require authorization for caching guilds Validate guild number Rename 'members' to 'guilds'

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const parser = require('body-parser');
 
 global.server = {};
 server.cfg = require('./config.json');
-server.members = new Map();
+server.guilds = new Map();
 
 app.use(express.static('./src/public'));
 app.use(parser.json());
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
     res.locals = {
         base: server.cfg.base,
         icons: server.cfg.iconRetreiver,
-        bots: server.cfg.bots.map((bot) => Object.assign({ members: server.members.has(bot.id) ? `${server.members.get(bot.id)}` : `No value cached yet!` }, bot)),
+        bots: server.cfg.bots.map((bot) => Object.assign({ guilds: server.guilds.has(bot.id) ? `${server.guilds.get(bot.id)} Guilds` : `No value cached yet!` }, bot)),
         version: require('./package.json').version
     };
     next();
@@ -31,7 +31,10 @@ app.get(`/`, (req, res) => {
 });
 
 app.post(`/bots/:id`, (req, res) => {
-    if (!req.body.members) return res.status(400).json({ error: `You have to provide a member count!` });
-    server.members.set(req.params.id, req.body.members);
-    res.status(200).json({ message: `Successfully cached member count!` });
+    if(req.headers.authorization != server.cfg.authorization) return res.status(401).json({ error: `You are not authorized to do this!` });
+    if(!server.cfg.bots.some((bot) => bot.id == req.params.id)) return res.status(400).json({ error: `This bot is not in our list!` });
+    if(req.body.guilds == null) return res.status(400).json({ error: `You have to provide a guild count!` });
+    if(typeof req.body.guilds != `number` || req.body.guilds < 0) return res.status(400).json({ error: `The guild count has to be a valid number above 0!` });
+    server.guilds.set(req.params.id, req.body.guilds);
+    res.status(200).json({ message: `Successfully cached guild count!` });
 });
